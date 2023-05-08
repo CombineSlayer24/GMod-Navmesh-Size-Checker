77ffdac7b9e212c43e7d822aba5f3442ebfd3f5b
CombineSlayer24 <luvangel157@aol.com>
2023-05-08 15:26:34 -0700

    fix indentations, stupid VSC

--- lua/autorun/navmesh_size_check.lua
+++ lua/autorun/navmesh_size_check.lua
@@ -2,26 +2,26 @@
 -- or for those that want to know what size they're aimming for GetSizeX / Y function
 
 hook.Add("PlayerSay", "NavmeshSizeCommand", function(ply, text, isTeam)
-    if string.lower( text ) == "!navsize" then
-        -- Use a trace and HitPos to get the current NavMesh we are looking at
-        local trace = ply:GetEyeTraceNoCursor()
-        local cursorPos = trace.HitPos
+	if string.lower( text ) == "!navsize" then
+		-- Use a trace and HitPos to get the current NavMesh we are looking at
+		local trace = ply:GetEyeTraceNoCursor()
+		local cursorPos = trace.HitPos
 		local navEdit = GetConVar( "nav_edit" ):GetBool()
-        local navArea = navmesh.GetNavArea( cursorPos, 30 )
+		local navArea = navmesh.GetNavArea( cursorPos, 30 )
 
 		if !navEdit then ply:PrintMessage( HUD_PRINTTALK, "nav_edit 1 is required!" ) return end
 
-        if navArea then
-            -- Print the size of the Navmesh area
-            local sizeX = navArea:GetSizeX()
-            local sizeY = navArea:GetSizeY()
-            ply:PrintMessage( HUD_PRINTTALK, "Navmesh area size: Width = " .. sizeX .. ", Length = " .. sizeY )
+		if navArea then
+			-- Print the size of the Navmesh area
+			local sizeX = navArea:GetSizeX()
+			local sizeY = navArea:GetSizeY()
+			ply:PrintMessage( HUD_PRINTTALK, "Navmesh area size: Width = " .. sizeX .. ", Length = " .. sizeY )
 
-        else
+		else
 
-            ply:PrintMessage( HUD_PRINTTALK, "No Navmesh detected or area not found under crosshairs." )
-        end
+			ply:PrintMessage( HUD_PRINTTALK, "No Navmesh detected or area not found under crosshairs." )
+		end
 
-        return ""
-    end
+		return ""
+	end
 end)
