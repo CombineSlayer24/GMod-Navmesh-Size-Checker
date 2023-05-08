bbdbbff604b95b349ef4157e74aaaaa476dd1f9f
CombineSlayer24 <luvangel157@aol.com>
2023-05-08 15:25:13 -0700

    Tidy up code

--- lua/autorun/navmesh_size_check.lua
+++ lua/autorun/navmesh_size_check.lua
@@ -2,15 +2,14 @@
 -- or for those that want to know what size they're aimming for GetSizeX / Y function
 
 hook.Add("PlayerSay", "NavmeshSizeCommand", function(ply, text, isTeam)
-    if string.lower(text) == "!navsize" then
-		
+    if string.lower( text ) == "!navsize" then
         -- Use a trace and HitPos to get the current NavMesh we are looking at
         local trace = ply:GetEyeTraceNoCursor()
         local cursorPos = trace.HitPos
 		local navEdit = GetConVar( "nav_edit" ):GetBool()
-        local navArea = navmesh.GetNavArea(cursorPos, 30)
+        local navArea = navmesh.GetNavArea( cursorPos, 30 )
 
-		if !navEdit then ply:PrintMessage(HUD_PRINTTALK, "nav_edit 1 is required!") return end
+		if !navEdit then ply:PrintMessage( HUD_PRINTTALK, "nav_edit 1 is required!" ) return end
 
         if navArea then
             -- Print the size of the Navmesh area
